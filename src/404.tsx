20977d7c7637a6a9d404210d57efffe51ba536e9
denkiuo604 <denkiuo604@gmail.com>
2023-11-25 09:15:20 +0900

    404 Not Foundページを作成

---
+++ src/404.tsx
@@ -0,0 +1,13 @@
+const NotFound = () => {
+  return (
+    <div className="App">
+      <h1>404 Not Found</h1>
+      <p>お探しのページが見つかりません。</p>
+      <p className="another-page">
+        <a href="/kotoribako-chatpalette/">チャットパレット加工ツールへ</a>
+      </p>
+    </div>
+  )
+}
+
+export default NotFound
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import ChatPalette from './ChatPalette'
 import CcfoliaFormat from './CcfoliaFormat'
+import NotFound from './404'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/kotoribako-chatpalette" element={<ChatPalette />} />
         <Route path="/kotoribako-chatpalette/ccfolia-format" element={<CcfoliaFormat />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   )
