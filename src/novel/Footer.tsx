edb42fdc53278ae6edfb1c7c24ebeec8914ace64
denkiuo604 <denkiuo604@gmail.com>
2024-05-13 23:04:38 +0900

    divタグがpタグの子要素とならないように修正

--- src/novel/Footer.tsx
+++ src/novel/Footer.tsx
@@ -2,13 +2,11 @@ const Footer = () => {
     return (
         <div>
             <p className="read-the-docs">
-                <div>
-                    本コンテンツは、
-                    <div>【小説『新約・コトリバコ』Amazon.co.jp限定特典PDF】</div>
-                    のデータを利用しています。
-                </div>
-                <a href="https://www.amazon.co.jp/dp/4048975862">https://www.amazon.co.jp/dp/4048975862</a>
-                <div>©手代木正太郎／まだら牛／KADOKAWA</div>
+                本コンテンツは、<br />
+                【小説『新約・コトリバコ』Amazon.co.jp限定特典PDF】<br />
+                のデータを利用しています。<br />
+                <a href="https://www.amazon.co.jp/dp/4048975862">https://www.amazon.co.jp/dp/4048975862</a><br />
+                ©手代木正太郎／まだら牛／KADOKAWA
             </p>
         </div>
     )
